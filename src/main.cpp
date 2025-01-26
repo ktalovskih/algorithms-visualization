80c90635cf2dc37e9380f1285dc788a2c2d19455
ashab_tamaev2 <ktalovskih@gmail.com>
2025-01-26 22:21:02 +0300

    commit

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <glad/glad.h>  // Include GLAD to load OpenGL functions
+#include <GLFW/glfw3.h> // Include GLFW for window and input management
+#include <iostream>
+
+int main(){
+    return 0;
+};
